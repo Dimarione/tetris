74c675277ffa4b16d4ec5113310dc0f3158d0aca
Dima Kolesnikov <Dimario.one@gmail.com>
2019-08-21 15:56:36 +0300

    changin from 21.08.19

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let tetris = document.createElement('div');
-tetris.classList.add('tetris');
+//создаем поля для тетриса
+let tetris = document.createElement('div'); //создаем поле
+tetris.classList.add('tetris'); //добавляем ему клвсс
 
+//заполняем поле ячейками
 for (let i = 1; i < 181; i++) {
     let excel = document.createElement('div');
     excel.classList.add('excel');
     tetris.appendChild(excel);
 }
 
-let main = document.getElementsByClassName('main')[0];
-main.appendChild(tetris);
+//создадим DOM элемент с классом main
+let main = document.getElementsByClassName('main')[0]; //указываем индекс элемента даже если он один
+main.appendChild(tetris); //добавляем в DOM элемент наше поле тетриса
 
+//получим все элементы с классом excel и запишем их в переменную
 let excel = document.getElementsByClassName('excel');
-let i = 0;
+let i = 0; //вспомогательная переменная
 
 for (var y = 18; y > 0; y--) {
     for (let x = 1; x < 11; x++) {
